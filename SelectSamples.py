b7543a8f9db9c3c223115e5e9b1073ab74c49996
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-07-01 17:30:32 +0200

    initial untested random sample selector

---
+++ SelectSamples.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+import glob
+import os
+import random
+import shutil
+
+# learning rate / 100 teilen
+# in standard yolo cfgs sind test daten einkommentiert
+# pytorch yolo nutzt adam stat dem standard stochachistic gradient descent
+# steps muss ich vlt. ändern, pytorch yolo hört nur auf epochen,
+#       flo meinte aber steps könnte ich wahrscheinlich gleich lassen
+# vlt data augmentation ausmachen (default müssen an, aber andere können vlt aus)
+
+class SampleSelector(ABC):
+    """Abstract class to select samples"""
+
+    def __init__(self, inputdir, outputdir):
+        self.inputdir = inputdir  # directory with images to choose from
+        self.outputdir = outputdir  # where list of selected images is located
+
+        self.trainImagesPool = []
+        self.findImages(self.inputdir)  # fill list with potential training images
+        # images are removed from the pool once they are labeled
+
+        self.trainImages = []  # labeled training images
+
+    @abstractmethod
+    def selectSamples(self, amount=100):
+        pass
+
+    def findImages(self, dir):
+        datasets = [x[0] for x in os.walk(dir)]  # a list of all subdirectories (including root directory)
+
+        for d in datasets:
+            self.trainImagesPool = glob.glob(f"{d}/*.png", recursive=True)
+            self.trainImagesPool += glob.glob(f"{d}/*.PNG", recursive=True)
+            self.trainImagesPool += glob.glob(f"{d}/*.jpg", recursive=True)
+            self.trainImagesPool += glob.glob(f"{d}/*.JPG", recursive=True)
+
+    def writeSamplesToFile(self):
+        """"
+        writes a list of the used samples to a file
+        """
+        samples = "\n".join(self.trainImages)
+        # with open(os.path.join(self.outputdir, "train.txt", "w")) as f:
+        #    f.write(samples)
+        with open(os.path.join(self.outputdir, "train.txt")) as f:
+            f.write(samples)
+
+    def copyFiles(self, toBeCopied):
+        """
+        This is currently not used, because we just define it via the train.txt
+        Double check if you want to use self.outputdir as the directory to copy images to if you use it
+        Copy n files to training folder
+        :param toBeCopied: list of files from the pool to be copied into training directory
+        :return: None
+        """
+        for source in toBeCopied:
+            # shutil.copy(source, self.outputdir)
+
+            labelFile = source.replace(".png", ".txt").replace(".jpg", ".txt")\
+                .replace(".PNG", ".txt").replace(".JPG", ".txt")
+            shutil.copy(labelFile, self.outputdir)
+            destination = os.path.join(self.outputdir, os.path.basename(source))
+            self.trainImages.append(destination)
+
+
+class RandomSampleSelector(SampleSelector):
+    """
+    Select samples randomly.
+    This is the baseline to compare other approaches to.
+    """
+
+    def __init__(self, inputdir, outputdir):
+        super().__init__(inputdir, outputdir)
+
+    def selectSamples(self, amount=5):
+        # selectedSamples = []
+        # for i in range(amount):
+            # sample = random.choice(self.trainImagesPool)
+            # selectedSamples.append(sample)
+            # self.trainImagesPool.remove(sample)
+        random.shuffle(self.trainImagesPool)
+        selectedSamples = self.trainImagesPool[:amount]
+        self.trainImages.extend(selectedSamples)
+        self.trainImagesPool = self.trainImagesPool[amount:]
+
+        # self.copyFiles(selectedSamples)
+        self.writeSamplesToFile()
+
+if __name__ == "__main__":
+    a = RandomSampleSelector("/homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/custom/images",
+                             "/homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/custom")
+    a.selectSamples()
