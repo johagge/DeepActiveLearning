b217d02f83d14a41ac4a11b2fd6d54ade9f36f1b
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-07-02 16:56:59 +0200

    yolo trainer: change params, run in loop

--- YoloTrainer.py
+++ YoloTrainer.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from pytorchyolo import utils, train
 from pytorchyolo import train
 import SelectSamples as samples
@@ -8,15 +9,21 @@ import SelectSamples as samples
 args = [sys.argv[0]]  # put python filename in sys.argv
 args += "--model /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/config/robocup.cfg".split(" ")
 args += "--data /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/robocup.data".split(" ")
-args += "--epochs 25".split(" ")
+args += "--epochs 5".split(" ")
 args += "--pretrained_weights /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/weights/yolov4-tiny.conv.29".split(" ")
 args += "--seed 42".split(" ")
 args += "--n_cpu 4".split(" ")
+args += "--evaluation_interval 1".split(" ")
 
-print(args)
 sys.argv = args  # overwrite sys argv with new arguments
 
+amount = 1000
+imagePoolSize = 8894# 8894 is the number of images in the trainings set of TORSO-21
 randomSamples = samples.RandomSampleSelector("/homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/custom/images",
                                              "/homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/custom")
-randomSamples.selectSamples(amount=1000)
-train.run()
+for run in range(math.ceil(amount / imagePoolSize)):
+    randomSamples.selectSamples(amount=1000)
+    train.run()
+    with open("log.txt", "a") as f:
+        f.write(f"_done with run: {run}")
+
