b4f75bac01fb69c1a6c34e4964b348a84f680ade
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-07-05 11:00:35 +0200

    hyperparameter changes

--- YoloTrainer.py
+++ YoloTrainer.py
@@ -9,23 +9,23 @@ import SelectSamples as samples
 args = [sys.argv[0]]  # put python filename in sys.argv
 args += "--model /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/config/robocup.cfg".split(" ")
 args += "--data /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/robocup.data".split(" ")
-args += "--epochs 5".split(" ")
+args += "--epochs 30".split(" ")
 args += "--pretrained_weights /homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/weights/yolov4-tiny.conv.29".split(" ")
 args += "--seed 42".split(" ")
-args += "--n_cpu 4".split(" ")
+args += "--n_cpu 10".split(" ")
 args += "--evaluation_interval 1".split(" ")
 
 sys.argv = args  # overwrite sys argv with new arguments
 
-amount = 1000
+amount = 3000
 randomSamples = samples.RandomSampleSelector("/srv/ssd_nvm/15hagge/torso-fuer-pytorchyolo/custom/images/train",
                                              "/homes/15hagge/deepActiveLearning/PyTorch-YOLOv3/data/")
 imagePoolSize = len(randomSamples.trainImagesPool)
 
 for run in range(math.ceil(imagePoolSize / amount)):
     print(f"___currently running {run} / {range(math.ceil(imagePoolSize / amount))}")
-    randomSamples.selectSamples(amount=1000)
+    randomSamples.selectSamples(amount=amount)
     train.run()
     with open("log.txt", "a") as f:
-        f.write(f"_done with run: {run}")
+        f.write(f"_done with run: {run}\n")
 
