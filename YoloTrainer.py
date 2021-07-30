9b5b3e485c51d34120176763d8e1e89bb58267ab
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-07-30 10:17:17 +0200

    add gaussian map mean to trainer

--- YoloTrainer.py
+++ YoloTrainer.py
@@ -63,6 +63,9 @@ elif trainer_args.mode == "most_bb":
 elif trainer_args.mode == "gaussian_mean_difference":
     sampler = samples.noiseSelector(inputdir, outputdir, trainImages=firstSamples[0],
                                     trainImagesPool=firstSamples[1], mode="gaussian_mean_difference")
+elif trainer_args.mode == "gaussian_map_mean":
+    sampler = samples.noiseSelector(inputdir, outputdir, trainImages=firstSamples[0],
+                                    trainImagesPool=firstSamples[1], mode="gaussian_map_mean")
 else:
     sys.exit("No or incorrect mode was provided")
 
