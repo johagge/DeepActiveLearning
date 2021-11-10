6db0291a5496548ef086919b2cc10348a79f2c9d
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-11-10 19:14:31 +0100

    graph by category

--- VisualizeResults.py
+++ VisualizeResults.py
@@ -9,6 +9,16 @@ import sys
 
 # TODO alternatively use only files specified via command line
 
+random = ['random']
+confidence_based = ['mean_confidence', 'mean_confidence_no_boxes', 'min_confidence', 'lowest_max_confidence']
+confidence_based2 = ['median', 'max']
+bb_amount = ['min_bb', 'most_bb']
+noise = ['gaussian_mean_difference', 'gaussian_map_mean', 'motion_blur_map_mean']
+img2vec = ['image_2_vec_resnet', 'vae']
+learnloss = ['learnloss']
+
+categories = [random, confidence_based, confidence_based2, bb_amount, noise, img2vec, learnloss]
+
 logdir = "log10"
 
 files = glob.glob(os.path.join(logdir, "*.txt"))
@@ -57,41 +67,48 @@ for key, value in results.items():
         data_in_columns = zip(*value)
         results[key] = data_in_columns
     """
+for i, cat in enumerate(categories):
 
-fig, ax = plt.subplots()
-used_images = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-used_images = np.array(used_images)
-used_images = used_images / 10
-for key, value in results.items():
-    if isinstance(value, pd.DataFrame):
-        #value = value.transpose()
-        #print(value)
-        #sns.lineplot(data=value, x=used_images, y=key)
-        #continue
-        # 95% confidence interval
-        ci = 1.96 * np.std(value) * np.mean(value)
-        print(key)
-        print(value[0])
-        y = [y for y in np.mean(value)]
-        print(y)
-        plt.fill_between(used_images, y - ci, y + ci, alpha=.3, label=key)
-    else:
-        ax.plot(used_images, value, "-o", label=key)
-ax.legend()
-plt.xlim(used_images[0], used_images[-1])  # use first and last element of used images as scale for x
-plt.ylim(0, 1)
-plt.xlabel("number of images used in training")
-plt.ylabel("mAP on test data")
+    fig, ax = plt.subplots()
+    used_images = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+    used_images = np.array(used_images)
+    used_images = used_images / 10
+    for key, value in results.items():
+        if key not in cat and key != 'random':
+            continue
+        if isinstance(value, pd.DataFrame):
+            #value = value.transpose()
+            #print(value)
+            #sns.lineplot(data=value, x=used_images, y=key)
+            #continue
+            # 95% confidence interval
+            ci = 1.96 * np.std(value) * np.mean(value)
+            print(key)
+            print(value[0])
+            y = [y for y in np.mean(value)]
+            print(y)
+            plt.fill_between(used_images, y - ci, y + ci, alpha=.3, label=key)
+        else:
+            ax.plot(used_images, value, "-o", label=key)
+    ax.legend()
+    plt.xlim(used_images[0], used_images[-1])  # use first and last element of used images as scale for x
+    plt.ylim(0, 1)
+    plt.xlabel("number of images used in training")
+    plt.ylabel("mAP on test data")
 
-# use different colors, because otherwise the colors are reused
-colormap = plt.cm.gist_ncar  # nipy_spectral, Set1,Paired
-# only up to 0.9, because 1 is almost white and hard to see
-colors = [colormap(i) for i in np.linspace(0, 0.9, len(ax.lines))]
-for i, j in enumerate(ax.lines):
-    j.set_color(colors[i])
+    '''
+    # use different colors, because otherwise the colors are reused
+    colormap = plt.cm.gist_ncar  # nipy_spectral, Set1,Paired
+    # only up to 0.9, because 1 is almost white and hard to see
+    colors = [colormap(i) for i in np.linspace(0, 0.9, len(ax.lines))]
+    for i, j in enumerate(ax.lines):
+        j.set_color(colors[i])
+    '''
 
-plt.show(dpi=200)
-# put the legend outside of graph
-# do it after showing the figure, because for some reason the bbox inches tight only works in savefig
-ax.legend(bbox_to_anchor=(1, 1))
-fig.savefig("graph.png", dpi=200, bbox_inches='tight')
+
+
+    plt.show(dpi=200)
+    # put the legend outside of graph
+    # do it after showing the figure, because for some reason the bbox inches tight only works in savefig
+    ax.legend(bbox_to_anchor=(1, 1))
+    fig.savefig(f"viz/{logdir}_{categories[i]}_graph.png", dpi=200, bbox_inches='tight')
