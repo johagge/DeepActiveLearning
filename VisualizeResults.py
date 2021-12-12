0269a8458d70279835ba2c87c44cd1264ef9c7f0
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-12-12 17:59:50 +0100

    use more human readable names for graphs

--- VisualizeResults.py
+++ VisualizeResults.py
@@ -19,7 +19,24 @@ learnloss = ['learnloss']
 
 categories = [random, confidence_based, confidence_based2, bb_amount, noise, img2vec, learnloss]
 
-logdir = "log10"
+category_names = {"random": "random sampling",
+                  "mean_confidence": "mean confidence",
+                  "mean_confidence_no_boxes": "mean confidence with no predictions",
+                  "min_confidence": "minimum confidence",
+                  "lowest_max_confidence": "lowest maximum confidence",
+                  "median": "median confidence",
+                  "max": "maximum confidence",
+                  "min_bb": "least predictions",
+                  "most_bb": "most predictions",
+                  "gaussian_mean_difference": "gaussian noise / mean difference",
+                  "gaussian_map_mean": "gaussian noise / mAP",
+                  "motion_blur_map_mean": "motion blur noise / mAP",
+                  "image_2_vec_resnet": "clustering",
+                  "vae": "Variational Autoencoder",
+                  "learnloss": "Learning Loss",
+                  }
+
+logdir = "log100"
 
 files = glob.glob(os.path.join(logdir, "*.txt"))
 print(files)
@@ -72,7 +89,7 @@ for i, cat in enumerate(categories):
     fig, ax = plt.subplots()
     used_images = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
     used_images = np.array(used_images)
-    used_images = used_images / 10
+    used_images = used_images
     for key, value in results.items():
         if key not in cat and key != 'random':
             continue
@@ -87,9 +104,9 @@ for i, cat in enumerate(categories):
             print(value[0])
             y = [y for y in np.mean(value)]
             print(y)
-            plt.fill_between(used_images, y - ci, y + ci, alpha=.3, label=key)
+            plt.fill_between(used_images, y - ci, y + ci, alpha=.3, label=category_names[key])
         else:
-            ax.plot(used_images, value, "-o", label=key)
+            ax.plot(used_images, value, "-o", label=category_names[key])
     ax.legend()
     plt.xlim(used_images[0], used_images[-1])  # use first and last element of used images as scale for x
     plt.ylim(0, 1)
