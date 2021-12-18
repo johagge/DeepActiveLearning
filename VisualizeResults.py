d26053cb644cb0529cfb7ad9f8e93f0ecd1455ec
Jonas Hagge <5hagge@informatik.uni-hamburg.de>
2021-12-18 14:03:00 +0100

    fixed colors, show mean+confidence interval

--- VisualizeResults.py
+++ VisualizeResults.py
@@ -7,8 +7,6 @@ import seaborn as sns
 import pandas as pd
 import sys
 
-# TODO alternatively use only files specified via command line
-
 random = ['random']
 confidence_based = ['mean_confidence', 'mean_confidence_no_boxes', 'min_confidence', 'lowest_max_confidence']
 confidence_based2 = ['median', 'max']
@@ -35,6 +33,23 @@ category_names = {"random": "random sampling",
                   "vae": "Variational Autoencoder",
                   "learnloss": "Learning Loss",
                   }
+
+colors = {"random": "red",
+          "mean_confidence": "blue",
+          "mean_confidence_no_boxes": "orange",
+          "min_confidence": "yellow",
+          "lowest_max_confidence": "magenta",
+          "median": "blue",
+          "max": "orange",
+          "min_bb": "blue",
+          "most_bb": "orange",
+          "gaussian_mean_difference": "blue",
+          "gaussian_map_mean": "orange",
+          "motion_blur_map_mean": "yellow",
+          "image_2_vec_resnet": "blue",
+          "vae": "orange",
+          "learnloss": "blue",
+          }
 
 logdir = "log100"
 
@@ -69,17 +84,35 @@ for file in files:
 # format the methods with multiple runs correctly with one tuple for each x
 for key, value in results.items():
     if isinstance(value[0], list):
+
         foo = pd.DataFrame(value)
         results[key] = foo
-        print(key)
-        print(foo)
+        # print(key)
+        # print(foo[0])
+
+        run_list = []
+        result_list = []
+        for n in range(0, 10):
+            for run in foo[n]:
+                run_list.append((n+1)*100)
+                result_list.append(run)
+        new_frame = pd.DataFrame(
+            {
+                "current_run": run_list,
+                "results": result_list
+            }
+        )
+        new_frame.columns = ['used_images', 'map_values']
+        foo = new_frame
+        results[key] = foo
+
         # for proper pandas dataframes:
         #   each column as one theme (e.g. random/vae)
         #   for multiple runs: first run, afterwards second run with same row names again
-        #std = np.std(foo)
-        #mean = np.mean(foo)
-        #print(mean)
-        #sys.exit()
+        # std = np.std(foo)
+        # mean = np.mean(foo)
+        # print(mean)
+        # sys.exit()
     """
         data_in_columns = zip(*value)
         results[key] = data_in_columns
@@ -91,23 +124,26 @@ for i, cat in enumerate(categories):
     used_images = np.array(used_images)
     used_images = used_images
     for key, value in results.items():
+        # print(key)
         if key not in cat and key != 'random':
             continue
         if isinstance(value, pd.DataFrame):
-            #value = value.transpose()
-            #print(value)
-            #sns.lineplot(data=value, x=used_images, y=key)
-            #continue
+            # value = value.transpose()
+            # print(value)
+            sns.lineplot(data=value, x='used_images', y='map_values', label=key, color=colors[key])# y=key)
+            continue  # previous way of plotting
             # 95% confidence interval
             ci = 1.96 * np.std(value) * np.mean(value)
-            print(key)
-            print(value[0])
+            # print(key)
+            # print(value[0])
             y = [y for y in np.mean(value)]
-            print(y)
+            # print(y)
             plt.fill_between(used_images, y - ci, y + ci, alpha=.3, label=category_names[key])
         else:
             ax.plot(used_images, value, "-o", label=category_names[key])
     ax.legend()
+    plt.grid()
+    plt.yticks(ticks=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
     plt.xlim(used_images[0], used_images[-1])  # use first and last element of used images as scale for x
     plt.ylim(0, 1)
     plt.xlabel("number of images used in training")
